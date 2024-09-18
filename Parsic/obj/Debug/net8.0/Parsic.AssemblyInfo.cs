179263c68544b4119cd17db782381fd259cbdf5e
Pavel Pontus <pavel_pontus@tut.by>
2024-09-18 19:03:54 +0300

    added readme

--- Parsic/obj/Debug/net8.0/Parsic.AssemblyInfo.cs
+++ Parsic/obj/Debug/net8.0/Parsic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Parsic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+358d67dc94e906a07a388e0d8cdb707fa4903530")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a1b6dfe0e6aa5e8e30850d7aa58a708391d2a03")]
 [assembly: System.Reflection.AssemblyProductAttribute("Parsic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Parsic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Rreadme.md
@@ -0,0 +1,10 @@
+#	Installation and first run
+
+* Setup SpecFlow extension for Visual Studio: `Extensions -> Manage Extensions -> Online -> SpecFlow for Visual Studio` download and install it.
+* build the project: `Build -> Build solution`
+* run cmd and execute command: "npm install playwright". then command: "npx playwright install" or "npm init playwright@latest" (during install select in options to reinstall browsers). if you don't have npm then you need to install node.js
+* Open test explorer `Test -> Test Explorer`, click `Run all tests`
+
+
+
+
